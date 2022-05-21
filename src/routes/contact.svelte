d3fd66fc568e672015f5fe0ff4ccc217cec430f7
Luke Shafer <hello@lukeshafer.com>
2022-05-21 00:03:14 -0400

    Start basic contact page layout

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,2 +1,53 @@
+<script lang="ts">
+    
+</script>
 
+<div class="page-wrapper">
+    <address>
+        <h2>Contact Info</h2>
+        <a id="email" href="mailto:hello@lukeshafer.com"><b>hello</b>@lukeshafer.com</a>
+        <a id="phone" href="tel:+16145853382">614-<b>LUKE-ETC</b></a>
+        <p></p>
+    </address>
+    <section class="form">
 
+    </section>
+</div>
+
+<style>
+    div.page-wrapper {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: flex-start;
+        gap: 1em 4em;
+        margin: 3em;
+    }
+
+    address {
+        background-color: rgb(var(--background-base));
+        padding: 1em 1.5em;
+        font-size: 2em;
+        font-style: normal;
+    }
+
+    h2 {
+        margin: 0;
+        text-align: center;
+    }
+
+    a {
+        display: block;
+        margin: 0;
+        font-family: inherit;
+        text-transform: inherit;
+        font-size: 0.8em;
+        color: rgb(var(--text-color));
+    }
+
+    a b {
+        color: rgb(var(--accent-color));
+    }
+
+</style>
