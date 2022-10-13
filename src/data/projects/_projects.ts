bab24738329930c4b57aff9ff5e1d459d7257c3e
Luke Shafer <hello@lukeshafer.com>
2022-10-12 21:04:53 -0400

    update portfolio

--- src/data/projects/_projects.ts
+++ src/data/projects/_projects.ts
@@ -1,20 +1,17 @@
 import type { SvelteComponentTyped } from 'svelte';
 import * as OnoTuroTuro from './onoturoturo.md';
-import * as GuardiansofHeart from './guardiansofheart.md';
-import * as LukeShaferCom from './lukeshafercom.md';
+// import * as GuardiansofHeart from './guardiansofheart.md';
+// import * as LukeShaferCom from './lukeshafercom.md';
 import * as EnnysWheels from './ennyswheels.md';
 import * as DoGoodCollective from './dogoodcollective.md';
-import * as FridgeMagnets from './fridgemagnets.md';
+// import * as FridgeMagnets from './fridgemagnets.md';
 
 /* To add additional .md files, just import above, and put into the below array 
     in the desired order for the website */
 const projects = [
-	FridgeMagnets,
 	OnoTuroTuro,
-	LukeShaferCom,
 	EnnysWheels,
 	DoGoodCollective,
-	GuardiansofHeart,
 ];
 
 export interface ProjectComponent {
